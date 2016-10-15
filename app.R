03bed701894833d2dd9b0b8001de4e18187fa231
Lek Hsiang Hui <xianghui83@gmail.com>
2016-10-16 00:55:01 +0800

    add k-means example

--- app.R
+++ app.R
@@ -176,6 +176,25 @@ ui <- shinyUI(navbarPage("BT2101 Demos",
                         DT::dataTableOutput('dtDT')
                       )
                     )
+               ),
+               
+               #for K-means
+               #taken from http://shiny.rstudio.com/gallery/kmeans-example.html
+               tabPanel("K-means",
+                        sidebarLayout(
+                          #left side bar
+                          sidebarPanel(
+                            selectInput('xcol', 'X Variable', names(iris)),
+                            selectInput('ycol', 'Y Variable', names(iris),
+                                        selected=names(iris)[[2]]),
+                            numericInput('clusters', 'Cluster count', 3,
+                                         min = 1, max = 9)
+                          ),
+                          #right main panel
+                          mainPanel(
+                            plotOutput('plot1')
+                          )
+                        )
                )
       )
 )
@@ -616,6 +635,27 @@ server <- shinyServer(function(input, output) {
   })
   
   #-------end decision tree-------
+  
+  #for kmeans
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+    
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  #-------end kmeans-------
   
   #TODO: more algos
 })
